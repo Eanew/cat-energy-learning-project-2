10a58be41881d8da22a26c9c1b7627b5c4c561d0
Eanew <H.Eanew@yandex.ru>
2020-01-28 21:12:46 +0300

    небольшие исправления

--- gulpfile.js
+++ gulpfile.js
@@ -116,6 +116,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     // "source/img/**",
+    "source/css/normalize.css",
     "source/*.ico"
   ], {
     base: "source"
